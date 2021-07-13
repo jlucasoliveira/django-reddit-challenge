198920bbe0fd2006a1cd77b5bb0dad29243cdaa1
jlucasoliveira <lucasosilva68@gmail.com>
2021-07-13 09:48:53 -0300

    fix: remove unnecessary commnet on settings

--- settings/settings.py
+++ settings/settings.py
@@ -21,10 +21,10 @@ dotenv.read_dotenv(os.path.join(BASE_DIR, ".env"))
 ENVIRONMENT = os.environ.get("ENVIRONMENT")
 LOAD_ENVS_FROM_FILE = True if os.environ.get("LOAD_ENVS_FROM_FILE", False) == "True" else False
 
-# env_file = "envs-production.json" if ENVIRONMENT == "production" else "envs-staging.json"
-# if not LOAD_ENVS_FROM_FILE:
-#     S3Environ(bucket="bucket-env", key=env_file)
-#     print("Loading envs from S3: {0}".format(env_file))
+env_file = "envs-production.json" if ENVIRONMENT == "production" else "envs-staging.json"
+if not LOAD_ENVS_FROM_FILE:
+    S3Environ(bucket="bucket-env", key=env_file)
+    print("Loading envs from S3: {0}".format(env_file))
 
 ###
 # Security
